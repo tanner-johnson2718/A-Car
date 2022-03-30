bd121568b7ecc64dc842fe2b7cd9c67d23510861
tanner-johnson2718 <tanner.johnson2718@gmail.com>
2022-03-29 18:09:48 -0800

    run script changes

--- run_car.sh
+++ run_car.sh
@@ -1,4 +1,4 @@
 cd mjpg-streamer
 sudo mjpg_streamer -i "input_uvc.so -d /dev/video0" -o "output_http.so -p 8090 -w /usr/local/share/mjpg-streamer/www" &
-python3 sensor_car.py &
+python3 ./sensor_car.py &
 cd ..
