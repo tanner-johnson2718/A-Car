d6fa53901ca098e1e5817431579933de6e3db359
tanner-johnson2718 <tanner.johnson2718@gmail.com>
2022-03-29 18:03:30 -0800

    run script changes

--- run_car.sh
+++ run_car.sh
@@ -1,3 +1,4 @@
 cd mjpg-streamer
-sudo mjpg_streamer -i "input_uvc.so -d /dev/video0" -o "output_http.so -p 8090 -w /usr/local/share/mjpg-streamer/www"
+sudo mjpg_streamer -i "input_uvc.so -d /dev/video0" -o "output_http.so -p 8090 -w /usr/local/share/mjpg-streamer/www" &
+python3 sensor_car.sh &
 cd ..
--- run_pc.sh
+++ run_pc.sh
@@ -1,1 +1,2 @@
-firefox http://192.168.8.1:8090/vid.html
+firefox http://192.168.8.1:8090/vid.html &
+python3 sensor_pc.py
