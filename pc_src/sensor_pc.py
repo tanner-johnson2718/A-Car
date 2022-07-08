e0921dbcc7356582f5d0ce4b4ac497fb46e6ae82
tanner <tanner.johnson2718@gmail.com>
2022-07-08 15:47:52 -0500

    adding ploting to sensor data reader on pc

--- pc_src/sensor_pc.py
+++ pc_src/sensor_pc.py
@@ -1,11 +1,30 @@
-# echo-client.py
+# Read json data from car sensor stream. Display data on terminal as it becomes
+# available.
 
+# import packages to read and parse data from sensor stream
 import socket
 import fcntl
 import json
 
+# import packages to display sensor data graphically
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+
+# sensor stream IP and port
 HOST = "192.168.8.1"
 PORT = 8091  # The port used by the server
+
+# create plot to display data
+fig, ((ax)) = plt.subplots(1,1)
+ax.set_ylim(bottom=-1,top=1)
+ax.set_xlim(left=-10,right=10)
+line, = ax.plot([], [], lw=3)
+ax.grid(1)
+
+plt.show()
+
+exit(0)
 
 print("Press Ctrl-C to exit")
 
